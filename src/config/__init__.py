72212bc142efe50ddcd846fbf028aad450cb987f
Jeremy-Qian <jeremytheking2013@gmail.com>
2026-05-28 10:30:47 +0800

    Clean up config manager: remove dead code, fix thread safety, clean up validation

---
+++ src/config/__init__.py
@@ -0,0 +1,11 @@
+from config.config_manager import ConfigManager, config_manager
+from config.tasks import TaskManager, task_manager
+from config import theme
+
+__all__ = [
+    "ConfigManager",
+    "config_manager",
+    "TaskManager",
+    "task_manager",
+    "theme",
+]
