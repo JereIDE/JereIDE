6a1dfb5c85be988ffcef426dfb5f58077515e44d
Jeremy-Qian <jeremytheking2013@gmail.com>
2026-07-15 16:37:25 +0800

    Update welcome page

--- crates/ui/src/welcome.rs
+++ crates/ui/src/welcome.rs
@@ -1,14 +1,31 @@
 use eframe::egui;
-use jereide_settings::{COMMAND_VIEW_FONT_SIZE, SURFACE_BG, TEXT_SECONDARY};
+use jereide_settings::{
+    COMMAND_VIEW_FONT_SIZE, EDITOR_FONT_SIZE, SURFACE_BG, TEXT_PRIMARY, TEXT_SECONDARY,
+};
 
 pub fn render_welcome_view(ui: &mut egui::Ui) {
     let rect = ui.max_rect();
     ui.painter().rect_filled(rect, 0.0, SURFACE_BG);
     ui.painter().text(
-        rect.center(),
+        egui::Pos2::new(rect.center().x - 110.0, rect.center().y + 13.0),
         egui::Align2::CENTER_CENTER,
-        "Welcome to JereIDE!",
+        "[LOGO]",
         egui::FontId::proportional(COMMAND_VIEW_FONT_SIZE),
+        TEXT_PRIMARY,
+    );
+    ui.painter().text(
+        egui::Pos2::new(rect.center().x - 70.0, rect.center().y),
+        egui::Align2::LEFT_CENTER,
+        "Welcome back to to JereIDE",
+        egui::FontId::proportional(COMMAND_VIEW_FONT_SIZE),
+        TEXT_PRIMARY,
+    );
+
+    ui.painter().text(
+        egui::Pos2::new(rect.center().x - 70.0, rect.center().y + 26.0),
+        egui::Align2::LEFT_CENTER,
+        "The editor for what's next",
+        egui::FontId::proportional(EDITOR_FONT_SIZE),
         TEXT_SECONDARY,
     );
 }
