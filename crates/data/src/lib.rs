be98e36f86895290f1da25ae65cc8bdc676d212e
Jeremy-Qian <jeremytheking2013@gmail.com>
2026-07-25 13:52:35 +0800

    Include data dir as well.

--- crates/data/src/lib.rs
+++ crates/data/src/lib.rs
@@ -1,6 +1,14 @@
 use std::path::PathBuf;
 
 pub fn data_dir() -> Option<PathBuf> {
+    if let Ok(exe) = std::env::current_exe() {
+        if let Some(dir) = exe.parent() {
+            let candidate = dir.join("data");
+            if candidate.is_dir() {
+                return Some(candidate);
+            }
+        }
+    }
     let mut dir = std::env::current_dir().ok()?;
     loop {
         let candidate = dir.join("data");
